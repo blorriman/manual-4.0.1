18965bce126f5ef452e75ea6adde9f4a70b431f7
Bob Lorriman <blorriman@gmail.com>
2021-11-09 10:53:15 -0800

    updated from 4.0.0

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,118 +1,85 @@
-// @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+	title: 'LORROS User Guide',
+	tagline: 'democratic decision making for boards and committees',
+	url: 'https://lorros.com',
+	baseUrl: '/',
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'warn',
+	favicon: 'img/favicon.ico',
+	organizationName: 'facebook', // Usually your GitHub org/user name.
+	projectName: 'docusaurus', // Usually your repo name.
+	themeConfig: {
+		colorMode: {
+			defaultMode: 'light',
+			disableSwitch: true,
+		},
+		navbar: {
+			logo: {
+				alt: 'LORROS Logo',
+				src: 'img/logo.png',
+			},
+			items: [
+				{
+					type: 'doc',
+					docId: 'intro',
+					position: 'left',
+					label: 'User Manual',
+				},
+				{
+					to: 'administration/intro',
+					activeBasePath: 'administration/intro',
+					label: 'Administrator Manual',
+					position: 'left',
+				},
+				{ to: '/blog', label: 'Best Practices', position: 'left' },
+			],
+		},
+		footer: {
+			style: 'dark',
+			copyright: `Copyright © ${new Date().getFullYear()} LORROS Web Applications Ltd.`,
+		},
+	},
+	presets: [
+		[
+			'@docusaurus/preset-classic',
+			{
+				docs: {
+					sidebarPath: require.resolve('./sidebars.js'),
+					remarkPlugins: [require('remark-import-partial')],
+					sidebarCollapsible: true,
+					// Please change this to your repo.
+					// editUrl:
+					// 	'https://github.com/facebook/docusaurus/edit/master/website/',
+				},
+				blog: {
+					showReadingTime: true,
+					blogSidebarTitle: 'Helpful Articles',
+					// Please change this to your repo.
+					// editUrl:
+					// 	'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+				},
+				// theme: {
+				// 	customCss: require.resolve('./src/css/custom.css'),
+				// },
+				theme: {
+					customCss: [require.resolve('./src/css/custom.css')],
+				},
+			},
+		],
+	],
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-
-/** @type {import('@docusaurus/types').Config} */
-const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
-};
-
-module.exports = config;
+	plugins: [
+		'@docusaurus/plugin-ideal-image',
+		[
+			'@docusaurus/plugin-content-docs',
+			{
+				id: 'administration',
+				path: 'administration',
+				routeBasePath: 'administration',
+				sidebarPath: require.resolve('./sidebarsAdmin.js'),
+				// ... other options
+			},
+		],
+	],
+}
